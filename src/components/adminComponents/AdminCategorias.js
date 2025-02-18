737c4df0f35b1d47aaef152f893b328b4dbc1404
JuanPineda09 <juanpineda420@gmail.com>
2025-02-17 22:58:40 -0500

    Se realiza GET de categoria

--- src/components/adminComponents/AdminCategorias.js
+++ src/components/adminComponents/AdminCategorias.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React,{ useState, useEffect} from "react";
 import { Outlet, NavLink } from 'react-router-dom';
+import crud from '../../../src/conexiones/crud'
 
 const AdminCategorias =()=>{
+
+    const [showCategories, setShowCategories] = useState([]);
+
+    const uploadCategorie = async ()=>{
+        const response = await crud.GET(`/api/categorias`)
+        setShowCategories(response);
+        console.log(response)
+    }
+
+    useEffect(()=>{
+        uploadCategorie();
+    },[]);
+
     return(
     <>
         <section className='bg-cyan-50 z-0 absolute w-4/5 h-[91.6%] right-0 top-14 flex flex-col overflow-scroll'>
@@ -16,16 +30,19 @@ const AdminCategorias =()=>{
                 <th>Fecha de Creación</th>
                 <th>Acciones</th>
             </tr>
-            <tr>
-                <td className='w-[10%]'>1</td>
-                <td className='w-[15%]'>Tecnologia</td>
-                <td className='w-1/4'>Lorem ipsum Lorem ipsum Lorem ipsum  Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</td>
+            {showCategories.map(category=>(
+                <tr key={category.idCategoria}>
+                <td className='w-[10%]'>{category.idCategoria}</td>
+                <td className='w-[15%]'>{category.nombre}</td>
+                <td className='w-1/4'>{category.descripcion}</td>
                 <td className='w-[30%]'>
-                    <img className='w-full h-full object-cover' src='https://imile.club/wp-content/uploads/2024/09/02-herramientas-tecnologicas-para-ecommerce.jpg'></img>
+                    <img className='w-full h-full object-cover' src={`http://localhost:4000/${category.imagen}`}></img>
                 </td>
                 <td className='w-[10%]'>Fecha de Creación</td>
                 <td className='w-[10%]'>Acciones</td>
-            </tr>
+                </tr>
+            ))}
+            
         </table>
 
         <NavLink to='/Dashboard-Administracion/AdminCategoriasCrear'>Crear Categoria</NavLink>
